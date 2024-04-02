264c7765e8b9315a84748fc80a41fe22e071e6aa
Léo Desktop <peyronnet.leo@gmail.com>
2024-04-02 17:47:54 +0200

    scroll-contact: + de une ancre

--- js/scroll-contact.js
+++ js/scroll-contact.js
@@ -1,19 +1,20 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // Sélectionne le lien d'ancrage
-    var ancre = document.getElementById('ancre-contact');
+    var ancres = document.querySelectorAll('.ancre-contact');
 
-    // Ajoute un écouteur d'événements pour le clic
-    ancre.addEventListener('click', function(event) {
-        // Empêche le comportement par défaut du lien (ne pas recharger la page)
-        event.preventDefault();
-
-        // Récupère la hauteur totale de la page
-        var hauteurPage = document.body.scrollHeight;
-
-        // Fait défiler la page jusqu'en bas
-        window.scrollTo({
-            top: hauteurPage,
-            behavior: 'smooth' // Défilement fluide
+    ancres.forEach(function(ancre){
+        ancre.addEventListener('click', function(event) {
+            // Empêche le comportement par défaut du lien (ne pas recharger la page)
+            event.preventDefault();
+    
+            // Récupère la hauteur totale de la page
+            var hauteurPage = document.body.scrollHeight;
+    
+            // Fait défiler la page jusqu'en bas
+            window.scrollTo({
+                top: hauteurPage,
+                behavior: 'smooth' // Défilement fluide
+            });
         });
     });
 });
+    
