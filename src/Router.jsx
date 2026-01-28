b37698dabbc756e0e3303117c4adb62823e7eab2
Léo Peyronnet <peyronnet.leo@gmail.com>
2026-01-28 11:51:08 +0100

    feat: restructure portfolio with new routing, layout components, and updated dependencies

---
+++ src/Router.jsx
@@ -0,0 +1,34 @@
+import { createBrowserRouter, Outlet, RouterProvider, useNavigation } from "react-router-dom";
+import Spinner from "./components/spinner/Spinner";
+import Header from "./layouts/header/Header";
+import Footer from "./layouts/footer/Footer";
+import HomePage from "./pages/home/HomePage";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Root><Outlet /></Root>,
+        children: [
+            {
+                path: "",
+                element: <HomePage />
+            },
+        ]
+    }
+]);
+
+function Root({ children }) {
+    const { state } = useNavigation();
+    return (
+        <>
+            <Header />
+            {state === "loading" ? <Spinner /> : children}
+            <Footer />
+        </>
+    );
+}
+
+export default function Router() {
+    return <RouterProvider router={router} />;
+}
+
